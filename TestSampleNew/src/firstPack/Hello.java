bb1464c365cacb1cd5495bc4fbe9c99c1e674d99
sudharsan.m <sudharsan.m@Laptop-OBS-117.optisolbusiness.com>
2021-07-02 16:26:08 +0530

    New Method added

--- TestSampleNew/src/firstPack/Hello.java
+++ TestSampleNew/src/firstPack/Hello.java
@@ -7,6 +7,15 @@ public class Hello {
 	{
 		System.out.println("Test Hello world");
 		System.out.println("Test second ");
+		
+		Hello h1 = new Hello();
+		h1.meth1();
+	}
+	
+	
+	public void meth1()
+	{
+		System.out.println("Test three ");
 	}
 
 }
